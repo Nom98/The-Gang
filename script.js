d45910a7a0ad89b64694aca318322bcf75a42629
Nom98 <72444153+Nom98@users.noreply.github.com>
2020-11-24 16:05:42 +0000

    Final changes in HTML and CSS
    also added JS

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('hey there!');
+
+
+
+function showpopup () {
+  var popup = document.getElementById('popup');
+  popup.classlist.add('show');
+}
+
+
+unction hidepopup () {
+  var popup = document.getElementById('popup');
+  popup.classlist.remove('show');
+}
